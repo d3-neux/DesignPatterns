9851895f3b1cbab0dbf4cbf3f5ab4f826d8b3be6
Daniel <d3_neux@live.com>
2026-01-27 22:05:32 -0500

    init

---
+++ DesignPatterns/CodeBuilder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Coding.Exercise
+{
+    public class CodeBuilder
+    {
+        public string ClassName { get; set; }
+        private Dictionary<string, string> FieldList = new Dictionary<string, string>();
+
+
+        public CodeBuilder(string className)
+        {
+            ClassName = className;
+        }
+
+        public CodeBuilder AddField(string name, string type)
+        {
+            FieldList.Add(name, type);
+            return this;
+        }
+
+        private string PrintFields()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var field in FieldList) {
+                sb.AppendLine($"  public {field.Value} {field.Key};");
+            }
+            return sb.ToString();
+        }
+
+        public override string ToString() {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine($"public class {ClassName}");
+            stringBuilder.AppendLine("{");
+            stringBuilder.Append(PrintFields());
+            stringBuilder.AppendLine("}");
+            return stringBuilder.ToString();
+        }
+
+        //static void Main(string[] args)
+        //{
+        //    CodeBuilder cb = new CodeBuilder("Person");
+        //    cb.AddField("Name", "string");
+        //    cb.AddField("Age", "int");
+
+        //    Console.WriteLine(cb.ToString());
+        //}
+    }
+}
